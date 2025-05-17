4299d1aba53e4bfbacc9faa732079d8b3dc92ae0
cohavcahana <cohavca@ac.sce.ac.il>
2025-05-17 17:07:58 +0300

    Added Initial Permutation (IP) step to preprocess plaintext for DES

--- des.py
+++ des.py
@@ -5,6 +5,20 @@ def binary_to_string(binary_str):
     chars = [binary_str[i:i+8] for i in range(0, len(binary_str), 8)]
     return ''.join([chr(int(char, 2)) for char in chars])
 
+IP = [
+    58, 50, 42, 34, 26, 18, 10, 2,
+    60, 52, 44, 36, 28, 20, 12, 4,
+    62, 54, 46, 38, 30, 22, 14, 6,
+    64, 56, 48, 40, 32, 24, 16, 8,
+    57, 49, 41, 33, 25, 17, 9,  1,
+    59, 51, 43, 35, 27, 19, 11, 3,
+    61, 53, 45, 37, 29, 21, 13, 5,
+    63, 55, 47, 39, 31, 23, 15, 7
+]
+
+def initial_permutation(binary_input):
+    return ''.join([binary_input[i - 1] for i in IP])
+
 if __name__ == "__main__":
     plaintext = "thoughts"
     key = "nonsense"
@@ -12,7 +26,15 @@ if __name__ == "__main__":
     binary_plaintext = string_to_binary(plaintext)
     binary_key = string_to_binary(key)
 
-    print("Plaintext (binary):", binary_plaintext)
-    print("Key (binary):", binary_key)
+    print("Plaintext:", binary_plaintext)
+    print("Key:", binary_key)
+
+ 
+    permuted_plaintext = initial_permutation(binary_plaintext)
+    print("After Initial Permutation:", permuted_plaintext)
+
+
+
+
 
         
